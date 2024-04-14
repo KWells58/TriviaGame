49edb686ebccb20e571bd0837959a7f102464a6a
RButcher0131 <rupertbutcher4572@gmail.com>
2024-04-14 17:18:08 -0400

    Quiz Questions Added

--- app/src/main/java/edu/floridapoly/securesoftware/spring24/triviagame/QuizDbHelper.java
+++ app/src/main/java/edu/floridapoly/securesoftware/spring24/triviagame/QuizDbHelper.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class QuizDbHelper extends SQLiteOpenHelper {
     // Database info
     private static final String DATABASE_NAME = "triviaQuiz.db";
-    private static final int DATABASE_VERSION = 6;
+    private static final int DATABASE_VERSION = 8;
 
     // Table and columns names
     private static final String TABLE_QUESTIONS = "questions";
