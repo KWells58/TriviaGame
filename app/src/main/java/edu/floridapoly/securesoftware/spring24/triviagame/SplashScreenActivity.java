86dffe9755f67f1b4247e08e0bcc315656f9d466
KWells58 <kelly.wells10000@gmail.com>
2024-04-15 12:14:40 -0400

    changed things to hopefully make it work

--- app/src/main/java/edu/floridapoly/securesoftware/spring24/triviagame/SplashScreenActivity.java
+++ app/src/main/java/edu/floridapoly/securesoftware/spring24/triviagame/SplashScreenActivity.java
@@ -1,10 +1,12 @@
 package edu.floridapoly.securesoftware.spring24.triviagame;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashScreenActivity extends AppCompatActivity {
 
     private static final long SPLASH_DELAY = 1500; // 1.5 seconds
